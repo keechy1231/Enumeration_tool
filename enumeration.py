45cb7eb265c102a60b08e848dee3d56d26faa6f1
keechy <keechy1231@hotmail.co.uk>
2022-08-19 23:33:50 +0100

    adding quiet

--- enumeration.py
+++ enumeration.py
@@ -45,7 +45,7 @@ def nmap_scan(host,outfile, quiet):
 	if quiet is False:
 		nmap = subprocess.call(['nmap','-sV','-sC' , '-oN',outfile,host], shell=False, stdin=None, stderr=None )
 	else:
-		nmap = subprocess.call(['nmap','-sV','-sC' , '-oN',outfile,host], shell=False, stdin=None, stderr=None, stdout=('/dev/null') )
+		nmap = subprocess.call(['nmap','-sV','-sC' , '-oN',outfile,host], shell=False, stdin=None, stderr=None, stdout=subprocess.DEVNULL )
 	nmap
 	
 def nmap_allports_scan(host,outfile,quiet):
@@ -53,7 +53,7 @@ def nmap_allports_scan(host,outfile,quiet):
 	if quiet is False:
 		nmap_allport= subprocess.call(['nmap' , '-oN' , outfile , host], shell=False, stdin=None, stderr=None )
 	else:
-		nmap_allport= subprocess.call(['nmap' , '-oN' , outfile , host], shell=False, stdin=None, stderr=None, stdout=('/dev/null') )
+		nmap_allport= subprocess.call(['nmap' , '-oN' , outfile , host], shell=False, stdin=None, stderr=None, stdout=subprocess.DEVNULL )
 	nmap_allport
 
 #Input arguments start here
